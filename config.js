6dcfb243d371f193d74d4969735b0698adf162f3
Kanal <kamalsonged@gmail.com>
2026-03-25 10:24:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -27,8 +27,8 @@
 
 const CONFIG = {
   // ── Recipient Info ────────────────────────────────────────────
-  name: "Irene",
-  photo: "./img/irene.jpg",       // Place your photo in the img/ folder
+  name: "dumeshwari",
+  photo: "",       // Place your photo in the img/ folder
   music: "./music/hbd.mpeg",      // Place your music in the music/ folder
 
   // ── Theme Colors ──────────────────────────────────────────────
